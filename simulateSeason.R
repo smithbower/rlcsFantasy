f93d147f71e3a54c6887b5b05d0533b629cb923b
Stephen Smithbower <smithy.s@gmail.com>
2019-04-16 00:10:16 -0600

    Compute min and max values for different attributes using 95% CI.

--- simulateSeason.R
+++ simulateSeason.R
@@ -24,7 +24,6 @@ estConf95 <- function(m, s, n)
 
 ## Params
 season <- 7
-numSimulations <- 1
 
 
 ## Load base player data from pre-season.
@@ -54,25 +53,48 @@ for (week in 1:length(weeklyPlayerData))
   
   for (pID in 1:nrow(data))
   {
+    weeklyPlayerData[[week]][pID, "ScoreMean"] <- data[pID, "Score"] / data[pID, "GP"]
+    weeklyPlayerData[[week]][pID, "ScoreStdv"] <- calcStdv(data[pID, "ScoreSqr"], data[pID, "Score"], data[pID, "GP"])
+    ci <- estConf95(weeklyPlayerData[[week]][pID, "ScoreMean"], weeklyPlayerData[[week]][pID, "ScoreStdv"], data[pID, "GP"])
+    weeklyPlayerData[[week]][pID, "ScoreMax"] <- ci[1]
+    weeklyPlayerData[[week]][pID, "ScoreMin"] <- ci[2]
+    
     weeklyPlayerData[[week]][pID, "GoalsMean"] <- data[pID, "Goals"] / data[pID, "GP"]
     weeklyPlayerData[[week]][pID, "GoalsStdv"] <- calcStdv(data[pID, "GoalsSqr"], data[pID, "Goals"], data[pID, "GP"])
+    ci <- estConf95(weeklyPlayerData[[week]][pID, "GoalsMean"], weeklyPlayerData[[week]][pID, "GoalsStdv"], data[pID, "GP"])
+    weeklyPlayerData[[week]][pID, "GoalsMax"] <- ci[1]
+    weeklyPlayerData[[week]][pID, "GoalsMin"] <- ci[2]
     
     weeklyPlayerData[[week]][pID, "AssistsMean"] <- data[pID, "Assists"] / data[pID, "GP"]
     weeklyPlayerData[[week]][pID, "AssistsStdv"] <- calcStdv(data[pID, "AssistsSqr"], data[pID, "Assists"], data[pID, "GP"])
+    ci <- estConf95(weeklyPlayerData[[week]][pID, "AssistsMean"], weeklyPlayerData[[week]][pID, "AssistsStdv"], data[pID, "GP"])
+    weeklyPlayerData[[week]][pID, "AssistsMax"] <- ci[1]
+    weeklyPlayerData[[week]][pID, "AssistsMin"] <- ci[2]
     
     weeklyPlayerData[[week]][pID, "SavesMean"] <- data[pID, "Saves"] / data[pID, "GP"]
     weeklyPlayerData[[week]][pID, "SavesStdv"] <- calcStdv(data[pID, "SavesSqr"], data[pID, "Assists"], data[pID, "GP"])
+    ci <- estConf95(weeklyPlayerData[[week]][pID, "SavesMean"], weeklyPlayerData[[week]][pID, "SavesStdv"], data[pID, "GP"])
+    weeklyPlayerData[[week]][pID, "SavesMax"] <- ci[1]
+    weeklyPlayerData[[week]][pID, "SavesMin"] <- ci[2]
     
     weeklyPlayerData[[week]][pID, "ShotsMean"] <- data[pID, "Shots"] / data[pID, "GP"]
     weeklyPlayerData[[week]][pID, "ShotsStdv"] <- calcStdv(data[pID, "ShotsSqr"], data[pID, "Shots"], data[pID, "GP"])
-    
-    weeklyPlayerData[[week]][pID, "AdjustedScoreMean"] <- data[pID, "AdjustedScore"] / data[pID, "GP"]
-    weeklyPlayerData[[week]][pID, "AdjustedScoreStdv"] <- calcStdv(data[pID, "AdjustedScoreSqr"], data[pID, "AdjustedScore"], data[pID, "GP"])
+    ci <- estConf95(weeklyPlayerData[[week]][pID, "ShotsMean"], weeklyPlayerData[[week]][pID, "ShotsStdv"], data[pID, "GP"])
+    weeklyPlayerData[[week]][pID, "ShotsMax"] <- ci[1]
+    weeklyPlayerData[[week]][pID, "ShotsMin"] <- ci[2]
   }
   
   ## Append expected scores for attacker, defender, and midfielder positions for each player.
   f <- apply(weeklyPlayerData[[week]][,c('GP', 'Score', 'Goals', 'Assists', 'Shots', 'Saves')], 1, function(x) scoreFunc(x[1], x[2], x[3], x[4], x[5], x[6]))
   f <- t(f)
-  
   weeklyPlayerData[[week]] <- cbind(weeklyPlayerData[[week]], attackScore = f[,1], midfieldScore = f[,2], defenderScore = f[,3])
+  
+  ## Append the minimum and maximum expected scores for attacker, defender, and midfielder positions for each player, based on 95% CI.
+  f <- apply(weeklyPlayerData[[week]][,c('GP', 'ScoreMax', 'GoalsMax', 'AssistsMax', 'ShotsMax', 'SavesMax')], 1, function(x) scoreFunc(1, x[2], x[3], x[4], x[5], x[6]))
+  f <- t(f)
+  weeklyPlayerData[[week]] <- cbind(weeklyPlayerData[[week]], maxAttackScore = f[,1], maxMidfieldScore = f[,2], maxDefenderScore = f[,3])
+  
+  f <- apply(weeklyPlayerData[[week]][,c('GP', 'ScoreMin', 'GoalsMin', 'AssistsMin', 'ShotsMin', 'SavesMin')], 1, function(x) scoreFunc(1, x[2], x[3], x[4], x[5], x[6]))
+  f <- t(f)
+  weeklyPlayerData[[week]] <- cbind(weeklyPlayerData[[week]], minAttackScore = f[,1], minMidfieldScore = f[,2], minDefenderScore = f[,3])
 }
