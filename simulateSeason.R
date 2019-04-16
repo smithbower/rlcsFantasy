cd766fa1c1f370d10700fdbf050a639029bf0c65
Stephen Smithbower <smithy.s@gmail.com>
2019-04-16 00:58:44 -0600

    Now pulls in pricing data from OG dataset, and uses weighted average of OG dataset and new data.

--- simulateSeason.R
+++ simulateSeason.R
@@ -41,7 +41,7 @@ for (i in 1:5)
 
 ## Compute weighting for each week + OG data.. want season data to be weighted
 ## 100% by the end of the season, so each week is worth 20%. OG data is worth
-## 100 - (0.2 & nWeek).
+## 100 - (0.2 * nWeek).
 ogDataWeight <- 1.0 - (0.2 * length(weeklyPlayerData))
 
 
@@ -53,31 +53,52 @@ for (week in 1:length(weeklyPlayerData))
   
   for (pID in 1:nrow(data))
   {
-    weeklyPlayerData[[week]][pID, "ScoreMean"] <- data[pID, "Score"] / data[pID, "GP"]
+    ogID <- which(as.character(playerScores$Player) == as.character(weeklyPlayerData[[week]][pID, "Player"]))
+    if (length(ogID) == 0)
+      next()
+    
+    if (week > 1) ## Keep running totals throughout the season.
+    {
+      weeklyPlayerData[[week]][pID, "GP"] <- weeklyPlayerData[[week - 1]][pID, "GP"] + weeklyPlayerData[[week]][pID, "GP"]
+      weeklyPlayerData[[week]][pID, "Score"] <- weeklyPlayerData[[week - 1]][pID, "Score"] + weeklyPlayerData[[week]][pID, "Score"]
+      weeklyPlayerData[[week]][pID, "ScoreSqr"] <- weeklyPlayerData[[week - 1]][pID, "ScoreSqr"] + weeklyPlayerData[[week]][pID, "ScoreSqr"]
+      weeklyPlayerData[[week]][pID, "Goals"] <- weeklyPlayerData[[week - 1]][pID, "Goals"] + weeklyPlayerData[[week]][pID, "Goals"]
+      weeklyPlayerData[[week]][pID, "GoalsSqr"] <- weeklyPlayerData[[week - 1]][pID, "GoalsSqr"] + weeklyPlayerData[[week]][pID, "GoalsSqr"]
+      weeklyPlayerData[[week]][pID, "Assists"] <- weeklyPlayerData[[week - 1]][pID, "Assists"] + weeklyPlayerData[[week]][pID, "Assists"]
+      weeklyPlayerData[[week]][pID, "AssistsSqr"] <- weeklyPlayerData[[week - 1]][pID, "AssistsSqr"] + weeklyPlayerData[[week]][pID, "AssistsSqr"]
+      weeklyPlayerData[[week]][pID, "Saves"] <- weeklyPlayerData[[week - 1]][pID, "Saves"] + weeklyPlayerData[[week]][pID, "Saves"]
+      weeklyPlayerData[[week]][pID, "SavesSqr"] <- weeklyPlayerData[[week - 1]][pID, "SavesSqr"] + weeklyPlayerData[[week]][pID, "SavesSqr"]
+      weeklyPlayerData[[week]][pID, "Shots"] <- weeklyPlayerData[[week - 1]][pID, "Shots"] + weeklyPlayerData[[week]][pID, "Shots"]
+      weeklyPlayerData[[week]][pID, "ShotsSqr"] <- weeklyPlayerData[[week - 1]][pID, "ShotsSqr"] + weeklyPlayerData[[week]][pID, "ShotsSqr"]
+    }
+    
+    weeklyPlayerData[[week]][pID, "price"] <- playerScores[ogID, "price"]
+    
+    weeklyPlayerData[[week]][pID, "ScoreMean"] <- (playerScores[ogID, "Score"] / playerScores[ogID, "G"] * 1.0 - (0.2 * week)) + (data[pID, "Score"] / data[pID, "GP"] * (0.2 * week))
     weeklyPlayerData[[week]][pID, "ScoreStdv"] <- calcStdv(data[pID, "ScoreSqr"], data[pID, "Score"], data[pID, "GP"])
     ci <- estConf95(weeklyPlayerData[[week]][pID, "ScoreMean"], weeklyPlayerData[[week]][pID, "ScoreStdv"], data[pID, "GP"])
     weeklyPlayerData[[week]][pID, "ScoreMax"] <- ci[1]
     weeklyPlayerData[[week]][pID, "ScoreMin"] <- ci[2]
     
-    weeklyPlayerData[[week]][pID, "GoalsMean"] <- data[pID, "Goals"] / data[pID, "GP"]
+    weeklyPlayerData[[week]][pID, "GoalsMean"] <- (playerScores[ogID, "Goals"] / playerScores[ogID, "G"] * 1.0 - (0.2 * week)) + (data[pID, "Goals"] / data[pID, "GP"] * (0.2 * week))
     weeklyPlayerData[[week]][pID, "GoalsStdv"] <- calcStdv(data[pID, "GoalsSqr"], data[pID, "Goals"], data[pID, "GP"])
     ci <- estConf95(weeklyPlayerData[[week]][pID, "GoalsMean"], weeklyPlayerData[[week]][pID, "GoalsStdv"], data[pID, "GP"])
     weeklyPlayerData[[week]][pID, "GoalsMax"] <- ci[1]
     weeklyPlayerData[[week]][pID, "GoalsMin"] <- ci[2]
     
-    weeklyPlayerData[[week]][pID, "AssistsMean"] <- data[pID, "Assists"] / data[pID, "GP"]
+    weeklyPlayerData[[week]][pID, "AssistsMean"] <- (playerScores[ogID, "Assists"] / playerScores[ogID, "G"] * 1.0 - (0.2 * week)) + (data[pID, "Assists"] / data[pID, "GP"] * (0.2 * week))
     weeklyPlayerData[[week]][pID, "AssistsStdv"] <- calcStdv(data[pID, "AssistsSqr"], data[pID, "Assists"], data[pID, "GP"])
     ci <- estConf95(weeklyPlayerData[[week]][pID, "AssistsMean"], weeklyPlayerData[[week]][pID, "AssistsStdv"], data[pID, "GP"])
     weeklyPlayerData[[week]][pID, "AssistsMax"] <- ci[1]
     weeklyPlayerData[[week]][pID, "AssistsMin"] <- ci[2]
     
-    weeklyPlayerData[[week]][pID, "SavesMean"] <- data[pID, "Saves"] / data[pID, "GP"]
-    weeklyPlayerData[[week]][pID, "SavesStdv"] <- calcStdv(data[pID, "SavesSqr"], data[pID, "Assists"], data[pID, "GP"])
+    weeklyPlayerData[[week]][pID, "SavesMean"] <- (playerScores[ogID, "Saves"] / playerScores[ogID, "G"] * 1.0 - (0.2 * week)) + (data[pID, "Saves"] / data[pID, "GP"] * (0.2 * week))
+    weeklyPlayerData[[week]][pID, "SavesStdv"] <- calcStdv(data[pID, "SavesSqr"], data[pID, "Saves"], data[pID, "GP"])
     ci <- estConf95(weeklyPlayerData[[week]][pID, "SavesMean"], weeklyPlayerData[[week]][pID, "SavesStdv"], data[pID, "GP"])
     weeklyPlayerData[[week]][pID, "SavesMax"] <- ci[1]
     weeklyPlayerData[[week]][pID, "SavesMin"] <- ci[2]
     
-    weeklyPlayerData[[week]][pID, "ShotsMean"] <- data[pID, "Shots"] / data[pID, "GP"]
+    weeklyPlayerData[[week]][pID, "ShotsMean"] <- (playerScores[ogID, "Shots"] / playerScores[ogID, "G"] * 1.0 - (0.2 * week)) + (data[pID, "Shots"] / data[pID, "GP"] * (0.2 * week))
     weeklyPlayerData[[week]][pID, "ShotsStdv"] <- calcStdv(data[pID, "ShotsSqr"], data[pID, "Shots"], data[pID, "GP"])
     ci <- estConf95(weeklyPlayerData[[week]][pID, "ShotsMean"], weeklyPlayerData[[week]][pID, "ShotsStdv"], data[pID, "GP"])
     weeklyPlayerData[[week]][pID, "ShotsMax"] <- ci[1]
@@ -87,14 +108,34 @@ for (week in 1:length(weeklyPlayerData))
   ## Append expected scores for attacker, defender, and midfielder positions for each player.
   f <- apply(weeklyPlayerData[[week]][,c('GP', 'Score', 'Goals', 'Assists', 'Shots', 'Saves')], 1, function(x) scoreFunc(x[1], x[2], x[3], x[4], x[5], x[6]))
   f <- t(f)
-  weeklyPlayerData[[week]] <- cbind(weeklyPlayerData[[week]], attackScore = f[,1], midfieldScore = f[,2], defenderScore = f[,3])
+  weeklyPlayerData[[week]] <- cbind(weeklyPlayerData[[week]], attackerScore = f[,1], midfieldScore = f[,2], defenderScore = f[,3])
   
   ## Append the minimum and maximum expected scores for attacker, defender, and midfielder positions for each player, based on 95% CI.
   f <- apply(weeklyPlayerData[[week]][,c('GP', 'ScoreMax', 'GoalsMax', 'AssistsMax', 'ShotsMax', 'SavesMax')], 1, function(x) scoreFunc(1, x[2], x[3], x[4], x[5], x[6]))
   f <- t(f)
-  weeklyPlayerData[[week]] <- cbind(weeklyPlayerData[[week]], maxAttackScore = f[,1], maxMidfieldScore = f[,2], maxDefenderScore = f[,3])
+  weeklyPlayerData[[week]] <- cbind(weeklyPlayerData[[week]], maxAttackerScore = f[,1], maxMidfieldScore = f[,2], maxDefenderScore = f[,3])
   
   f <- apply(weeklyPlayerData[[week]][,c('GP', 'ScoreMin', 'GoalsMin', 'AssistsMin', 'ShotsMin', 'SavesMin')], 1, function(x) scoreFunc(1, x[2], x[3], x[4], x[5], x[6]))
   f <- t(f)
-  weeklyPlayerData[[week]] <- cbind(weeklyPlayerData[[week]], minAttackScore = f[,1], minMidfieldScore = f[,2], minDefenderScore = f[,3])
+  weeklyPlayerData[[week]] <- cbind(weeklyPlayerData[[week]], minAttackerScore = f[,1], minMidfieldScore = f[,2], minDefenderScore = f[,3])
+  
+  
+  ## Remove any players that don't have OG data.
+  weeklyPlayerData[[week]] <- weeklyPlayerData[[week]][complete.cases(weeklyPlayerData[[week]]),]
+}
+
+
+## #############################################################################
+## OPTIMIZATION                                                               ##
+## #############################################################################
+selectTeam <- function(initialTeam, week, ogData, weeklyData)
+{
+  ## Compute new expected points using weighted averages.
+  weightedData <- setNames(data.frame(matrix(ncol = 5, nrow = 0)), 
+                                      c("Player", "attackerScore", "defenderScore", "midfieldScore", "Price"))
+  
+  for (i in 1 : nrows(ogData))
+  {
+    weightedData
+  }
 }
